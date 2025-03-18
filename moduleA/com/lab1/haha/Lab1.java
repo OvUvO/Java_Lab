bd3996a465bcf0abddadb12ccdf1f8bcff67d970
OvHvO <waikitpua1128@gmail.com>
2025-03-19 07:57:03 +0800

    Solve 3/19

--- moduleA/com/lab1/haha/Lab1.java
+++ moduleA/com/lab1/haha/Lab1.java
@@ -3,7 +3,6 @@ package com.lab1.haha;
 public class Lab1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("My first java program !!!");
 	}
 
