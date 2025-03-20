973286c8977f891182f8b16a15271d4cb5ae88b7
OvHvO <waikitpua1128@gmail.com>
2025-03-20 16:24:39 +0800

    Lab7 cp

--- moduleA/com/lab7/haha/Lab7_Q2.java
+++ moduleA/com/lab7/haha/Lab7_Q2.java
@@ -1,10 +1,60 @@
 package com.lab7.haha;
 
+interface GeometricObject{
+	
+	public double getPerimeter();
+	public double getArea();
+	
+}
+
+interface Resizable{
+	
+	public boolean resize(int percent);
+	
+}
+
+class Circle implements GeometricObject{
+	
+	double radius;
+	
+	public Circle(double radius) {
+		this.radius = radius;
+	}
+	
+	@Override
+	public double getPerimeter() {
+		return (2*3.14*radius);
+	}
+	
+	@Override
+	public double getArea() {
+		return (3.14*radius*radius);
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("Perimeter: %2f, Area: %2f",getPerimeter(),getArea());
+	}
+	
+}
+
+class ResizableCircle implements Resizable{
+	int percent = 1;
+	
+	public ResizableCircle(double radius) {
+
+	}
+	
+	@Override
+	public boolean resize(int percent) {
+		return true;
+	}
+}
 public class Lab7_Q2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Circle obj = new Circle(3);
+		System.out.println(obj);
 	}
 
 }
